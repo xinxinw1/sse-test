7798bfdc5b054802f67d1b2e0861572ecce87f72
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2017-01-31 11:19:48 -0500

    initial files

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + '/'));
+
+var testdata = "This is my message";
+
+app.get('/connect', function(req, res){
+    res.writeHead(200, {
+      'Connection': 'keep-alive',
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache'
+    });
+
+    setInterval(function(){
+      console.log('writing ' + testdata);
+      res.write('data: {"msg": '+ testdata +'}\n\n');
+    }, 1000);
+});
+
+var port = process.env.PORT || 8003;
+app.listen(port, function() {
+  console.log("Running at Port " + port);
+});
+
